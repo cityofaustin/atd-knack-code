f3726d3a1ab9b85c4e551d60e32f0bb687db9d76
John Clary <john.clary@austintexas.gov>
2018-08-14 23:22:59 -0500

    Item Copying
    Add functionality to allow user to copy items. When a user submits the "copy item" form, the form inserts a copied record connected to the same purchase request.

--- finance_admin_portal/finance-admin-portal.js
+++ finance_admin_portal/finance-admin-portal.js
@@ -8,14 +8,15 @@ function changeFieldColor(field, color_map){
 
 var colorMapOne = {
    "Not Submitted" : { "background_color" : "#ff9b9c", "color" : "#fff" },
-   "Returned" : { "background_color" : "#E41A1C", "color" : "#fff" },
+   "Returned" : { "background_color" : "#ff9b9c", "color" : "#fff" },
    "Rejected" : { "background_color" : "#6a6565", "color" : "#fff" },
-   "Cancelled" : { "background_color" : "#adadad", "color" : "#fff" },
+   "Cancelled" : { "background_color" : "#6a6565", "color" : "#fff" },
    "Waiting for Approval": { "background_color" : "#377eb8", "color" : "#fff" },
-    "Purchase Review" : { "background_color" : "#99d8c9", "color" : "#fff" },
+    "Purchase Review" : { "background_color" : "#41ae76", "color" : "#fff" },
     "Budget Review" : { "background_color" : "#41ae76", "color" : "#fff" },
-    "Processing | Purchasing" : { "background_color" : "#006d2c", "color" : "#fff" },
-    "Processing | Accounts Payable" : { "background_color" : "#006d2c", "color" : "#fff" },
+    "Processing | Purchasing" : { "background_color" : "#41ae76", "color" : "#fff" },
+    "Pending Invoice" : { "background_color" : "#f5901f", "color" : "#fff" },
+    "Processing | Accounts Payable" : { "background_color" : "#41ae76", "color" : "#fff" },
     "Closed" : { "background_color" : "#ffffff", "color" : "#fff" },
 }
 
@@ -60,6 +61,7 @@ $(document).on('knack-page-render.scene_68', function(event, page) {
     $("#viewPR").append("<a class='big-button' href='https://atd.knack.com/finance-admin#purchase-requests/'><div class='big-button-container'><span><i class='fa fa-search'></i></span><span> View Request details </span></div></a>");
     hideDetailsLink("viewPR", "field_11");
   
+  
   //  Remove unwanted select options from approval authority list
   $("option[value='8 | Budget Review']").remove();
   $("option[value='7 | Purchase Review']").remove();
@@ -72,3 +74,68 @@ function hideDetailsLink(dest_id, src_field) {
     $(".kn-details-link." + src_field).remove();
 }
 
+
+
+// --- Begin Item Copying ---
+$(document).on('knack-view-render.view_315', function(event, view) {
+    // automatically submit 'copy' form when modal renders
+    $('button[type=submit]').submit();
+});
+
+$(document).on('knack-form-submit.view_315', function(event, view, record) {
+    // Insert a copy of an item to the same purchase request
+    var url = 'https://api.knack.com/v1/pages/scene_123/views/view_316/records/';
+
+    fields = [
+        'field_36', // unit of measure
+        'field_37', // part #
+        'field_15', // description
+        'field_16', // unit cost 
+        'field_20_raw', // purchase request
+        'field_189_raw', // department
+        'field_105_raw', // fund
+        'field_103_raw', // unit
+        'field_104_raw', // object
+    ];
+
+    // reduce object to specified fields
+    const filtered = Object.keys(record)
+      .filter(key => fields.includes(key))
+      .reduce((obj, key) => {
+        var new_key = key.replace('_raw', '')
+        obj[new_key] = record[key];
+        return obj;
+      }, {});
+
+    console.log(filtered);
+
+    insertRecord(filtered, url);
+
+});
+
+
+function insertRecord(record, url) {
+    
+    Knack.showSpinner();
+
+    var user = Knack.getUserToken();
+
+    var headers = {
+        'X-Knack-Application-ID': '5b422c9b13774837e54ed814',
+        'Authorization': user,
+        'content-type':'application/json'
+    };
+
+    // insert the record
+    $.ajax({
+        url: url,
+        type: 'POST',
+        headers: headers,
+        data:  JSON.stringify(record),
+        success: function(response) {
+          Knack.hideSpinner();
+        }
+    });
+
+}
+// --- End Item Copying ---
