038d90e7a93726c4bb91302297f7bc78ba8eda0d
John Clary <john.clary@austintexas.gov>
2018-08-19 12:35:56 -0500

    Redirect to PR Details on Item Copy Complete
    When an item is copied, use the AJAX callback to manually re-direct to the PR details page. This prevents the user from returning to the PR details page before the copy is complete

--- finance_admin_portal/finance-admin-portal.js
+++ finance_admin_portal/finance-admin-portal.js
@@ -162,15 +162,12 @@ $(document).on('knack-view-render.view_315', function(event, view) {
 
 $(document).on('knack-form-submit.view_315', function(event, view, record) {
     // Insert a copy of an item to the same purchase request
-
     var formUrl = "https://api.knack.com/v1/pages/scene_123/views/view_316/records/";
 
     // url where to redirect to on record insert success
     var redirectUrl = "https://atd.knack.com/finance-admin#purchase-requests/purchase-request-details/";
     
     // grab ID of purchase request and append it to redirect URL
-    console.log(record.field_20_raw);
-
     redirectUrl = redirectUrl + record.field_20_raw[0].id;
 
     console.log(redirectUrl);
