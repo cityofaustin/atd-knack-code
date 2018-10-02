517a4a0607f6ab33bb6bdad6547aa1025c48af88
John Clary <john.clary@austintexas.gov>
2018-10-01 23:49:19 -0500

    IE Detection / Alert

--- finance_admin_portal/finance-admin-portal.js
+++ finance_admin_portal/finance-admin-portal.js
@@ -257,4 +257,39 @@ $(document).on('knack-form-submit.view_285', function(event, view, record) {
 });
 
 
+/**
+ * detect IE
+ * returns version of IE or false, if browser is not Internet Explorer
+ * https://stackoverflow.com/questions/19999388/check-if-user-is-using-ie-with-jquery
+ */
+function detectIE() {
+    var ua = window.navigator.userAgent;
 
+    var msie = ua.indexOf('MSIE ');
+    if (msie > 0) {
+        // IE 10 or older => return version number
+        return parseInt(ua.substring(msie + 5, ua.indexOf('.', msie)), 10);
+    }
+
+    var trident = ua.indexOf('Trident/');
+    if (trident > 0) {
+        // IE 11 => return version number
+        var rv = ua.indexOf('rv:');
+        return parseInt(ua.substring(rv + 3, ua.indexOf('.', rv)), 10);
+    }
+
+    var edge = ua.indexOf('Edge/');
+    if (edge > 0) {
+       // Edge (IE 12+) => return version number
+       return parseInt(ua.substring(edge + 5, ua.indexOf('.', edge)), 10);
+    }
+
+    // other browser
+    return false;
+}
+
+$(document).on('knack-scene-render.scene_1', function() {
+  if (detectIE()) {
+    alert("You are using Microsoft Internet Explorer or Edge to view this page. We recommend using Firefox or Chrome. Contact your system administrator for assistance.");
+  }
+});
