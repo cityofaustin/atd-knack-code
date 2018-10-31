0e38406cc01e960802d80912f485d28011184f48
John Clary <john.clary@austintexas.gov>
2018-10-31 13:19:19 -0500

    Update dts-portal.js

--- dts_portal/dts-portal.js
+++ dts_portal/dts-portal.js
@@ -1,6 +1,5 @@
-
-function changeFieldColor(field, color_map){
-  var child_field = $(field).find('.kn-detail-body');
+function changeFieldColor(fieldClass, color_map){
+  var child_field = $(fieldClass).find('.kn-detail-body');
   var value = child_field.text()
   if (color_map[value]) {
     $(child_field).css({'background-color' : color_map[value].background_color, 'color': color_map[value].color });
@@ -8,114 +7,167 @@ function changeFieldColor(field, color_map){
 }
 
 
+function insertIcon(fieldClass, icon_map){
+  var child_field = $(fieldClass).find('.kn-detail-body');
+  var value = child_field.text()
+  var elem = $(".kn-detail" + fieldClass).find(".kn-detail-body").find("span")[0];
+
+  $(elem).before("<span> <i class='fa fa-" + icon_map[value].icon + "'></i> </span>");
+}
+
+
 var colorMapOne = {
-   'New' : { "background_color" : "#fb9a99", "color" : "#000" },
-   'In Progress': { "background_color" : "#1f78b4", "color" : "#fff" },
+   "Not Submitted" : { "background_color" : "#ff9b9c", "color" : "#fff", 'icon': null },
+   "Returned" : { "background_color" : "#ff9b9c", "color" : "#fff", 'icon': null },
+   "Rejected" : { "background_color" : "#6a6565", "color" : "#fff", 'icon': null },
+   "Cancelled" : { "background_color" : "#6a6565", "color" : "#fff", 'icon': null },
+   "Waiting for Approval": { "background_color" : "#377eb8", "color" : "#fff", 'icon': null },
+    "Purchase Review" : { "background_color" : "#41ae76", "color" : "#fff", 'icon': 'clipboard' },
+    "Budget Review" : { "background_color" : "#41ae76", "color" : "#fff", 'icon': 'money' },
+    "Processing | Purchasing" : { "background_color" : "#41ae76", "color" : "#fff", 'icon': 'cogs' },
+    "Pending Invoice" : { "background_color" : "#f5901f", "color" : "#fff", 'icon': 'clock-o' },
+    "Processing | Accounts Payable" : { "background_color" : "#41ae76", "color" : "#fff", 'icon': 'credit-card' },
+    "Closed" : { "background_color" : "#ffffff", "color" : "#000", 'icon': 'check-circle' },
+    "Unpaid" : { "background_color" : "#ff9b9c", "color" : "#fff", 'icon': null },
+    "Paid" : { "background_color" : "#41ae76", "color" : "#fff", 'icon': null },
+    "Submitted to Accounts Payable" : { "background_color" : "#f5901f", "color" : "#fff", 'icon': null },
 }
 
-$(document).on('knack-scene-render.any', function() {
-  //  Set status color
-  changeFieldColor('.field_91', colorMapOne);
+$(document).on('knack-scene-render.scene_4', function() {
+  //  work orders signs/markings status
+  changeFieldColor('.field_17', colorMapOne);
+  insertIcon('.field_17', colorMapOne);
   
-})
-
-function hideFieldIfNotRole(field_id, role_object_id) {
-  //  function to hide a field based on if the user does not have a given role
-  if ( !Knack.getUserRoles(role_object_id) ) {
-    $('#' + field_id).hide();
-  }
-}
-
-function hideViewIfNotRole(view_id, role_object_id) {
-  //  function to hide a view based on if the user does not have a given role
-  if ( !Knack.getUserRoles(role_object_id) ) {
-    $('#' + view_id).hide();
-  }
-}
-
-$(document).on('knack-view-render.view_76', function(event, page) {
-  hideFieldIfNotRole('kn-input-field_91', 'object_5'); // status (defaults to new)
-  hideFieldIfNotRole('kn-input-field_96', 'object_5'); // requester account (we set this automatically)
-  hideFieldIfNotRole('kn-input-field_97', 'object_5'); // assigned to (set by editors only)
-  hideFieldIfNotRole('kn-input-field_153', 'object_5'); // assigned to (set by editors only)
-  hideFieldIfNotRole('kn-input-field_178', 'object_5'); // division (set automatically by logged-in account)
-  hideFieldIfNotRole('kn-input-field_179', 'object_5'); // est. level of effort 
 });
-
-
-
-
-$(document).on('knack-view-render.view_114', function(event, page) {
-  // hide "Assigned to Me" service request table
-  hideViewIfNotRole('view_114', 'object_5');
-});
-
   
-function bigButton(div_id, view_id, url, fa_icon, button_label, callback) {
-  // create a large button
+
+
+
+function customButton(div_id, view_id, url, fa_icon, button_label, button_class, container_class, callback) {
+  // create a custom button
   
     $("<div/>", {
       id: div_id,
     }).appendTo("#" + view_id);
     
-  $("#" + div_id).append("<a class='big-button' href='" + url + "'><div class='big-button-container'><span><i class='fa fa-" + fa_icon + "'></i></span><span> " + button_label + "</span></div></a>");
+  $("#" + div_id).append("<a class='" + button_class + "' href='" + url + "'><div class='" + container_class + "'><span><i class='fa fa-" + fa_icon + "'></i></span><span> " + button_label + "</span></div></a>");
 
   if(callback) callback();
 }
 
 
+
+$(document).on('knack-view-render.view_167', function(event, page) {
+  // create large button on the home page
+    customButton(
+        "all",
+        "view_167",
+        "https://atd.knack.com/finance-purchasing#purchase-requests/",
+        "archive",
+        "All Purchase Requests",
+        "big-button",
+        "big-button-container"
+    );
+
+    customButton(
+        "create",
+        "view_167",
+        "https://atd.knack.com/finance-purchasing#new-purchase-requests/",
+        "plus-circle",
+        "New Purchase Request",
+        "big-button",
+        "big-button-container"
+    );
+
+    customButton(
+        "review",
+        "view_167",
+        "https://atd.knack.com/finance-purchasing#reviews/",
+        "check-square-o",
+        "Review Purchase Requests",
+        "big-button",
+        "big-button-container"
+    );
+
+    customButton(
+        "my",
+        "view_167",
+        "https://atd.knack.com/finance-purchasing#my-purchase-requests/",
+        "male",
+        "My Purchase Requests",
+        "big-button",
+        "big-button-container"
+    );
+
+});
+
+$(document).on('knack-page-render.scene_68', function(event, page) {
+  // render Review Details page
+  
+  //  Create big PR details button and hide the small link
+  customButton(
+      "viewPR",
+      "view_247",
+      "https://atd.knack.com/finance-purchasing#purchase-requests/",
+      "list-alt",
+      "View Request Details",
+      "big-button",
+      "big-button-container"
+  );
+
+  hideDetailsLink("viewPR", "field_11");
+
+  
+  //  Remove unwanted select options from approval authority list
+  $("option[value='8 | Budget Review']").remove();
+  $("option[value='7 | Purchase Review']").remove();
+});
+
+
+function hideDetailsLink(dest_id, src_field) {
+    var detailsUrl = $(".kn-link-page").attr("href");
+    $("#" + dest_id).find("a").attr("href", detailsUrl);
+    $(".kn-details-link." + src_field).remove();
+}
+
+
+
 function customLoginButton(view_id, page_name) {
   //  special logic to generate URL and clean-up sign in page brefore creating large button
     $('.kn-sso-container').hide();
+
     $('.login_form').hide();
 
     $('h2.kn-title').hide();
     
-    var url ="https://atd.knack.com/dts#" + page_name + "/auth/COACD";
+    var url ="https://atd.knack.com/finance-purchasing#" + page_name + "/auth/COACD";
 
-    bigButton('caocd-button-login', view_id, url, 'sign-in', 'Sign-In');
+    customButton(
+      'caocd-button-login',
+      view_id, url,
+      'sign-in',
+      'Sign-In',
+      "big-button",
+      "big-button-container"
+    );
 
-    bigButton('non-coacd-button-login', view_id, "javascript:void(0)", 'sign-in', 'Sign-In (Non-COA)', function(divId='non-coacd-button-login') {
-      setClickEvent(divId, showHideElements, ".login_form", ".big-button-container");
-    });
-
+    customButton(
+      'non-coacd-button-login',
+      view_id,
+      "javascript:void(0)",
+      'lock',
+      'Non-COA Sign-In',
+      "small-button",
+      "small-button-container",
+      function(divId='non-coacd-button-login') {
+        setClickEvent(
+          divId,
+          showHideElements,
+          ".login_form",
+          ".small-button-container,.big-button-container"
+        );
+      });
 }
-
-
-$(document).on('knack-view-render.any', function(event, page) {
-    //  wrapper to create lare sign-in buttons
-    // specify "rich text" form elements here
-    // the rich text is created on the login page and is a placeholder
-    // for the button div, and must link to the appropriate URL name setting
-    var views = {
-        'view_119' : 'datasets',
-        'view_120' : 'applications',
-        'view_122' : 'service-requests',
-        'view_123' : 'edit-service-request',
-        'view_124' : 'dts-team-portal',
-        'view_125' : 'admin',
-    }
-
-    if (page.key in views) {
-        customLoginButton(page.key, views[page.key]);    
-    }
-    
-});
-
-$(document).on('knack-view-render.view_127', function(event, page) {
-  // create large button on the home page
-    bigButton('service-requests', 'view_127', "https://atd.knack.com/dts#service-requests/", "phone-square", "Service Requests");
-});
-
-$(document).on('knack-view-render.view_128', function(event, page) {
-    // create large button on the home page
-    bigButton('data-inventory', 'view_128', "https://atd.knack.com/dts#datasets/", "database", "Data Inventory");
-});
-
-$(document).on('knack-view-render.view_146', function(event, page) {
-  // create large button on the home page
-    bigButton('team-trello', 'view_146', "https://trello.com/b/pNbgaKme/data-technology-services-operations", "trello", "Operations Board");
-});
 
 function setClickEvent(divId, func, param1, param2) {
   // TODO make these args less weird
@@ -128,3 +180,163 @@ function showHideElements(showSelector, hideSelector) {
   $(showSelector).show();
   $(hideSelector).hide();
 }
+
+$(document).on('knack-view-render.any', function(event, page) {
+    //  wrapper to create large sign-in buttons
+    //  the views ojbect uses the view id of the login form element as each key
+    //  and the page url of the login page's **chile page** as the value
+    var views = {
+        'view_39' : 'home',
+        'view_5' : 'purchase-requests',
+        'view_82' : 'purchasing-budget-review',
+        'view_52' : 'account-administration',
+        'view_322' : 'commodity-codes',
+        'view_31' : 'reviews',
+        'view_387' : 'invoice-details',
+        'view_379' : 'add-invoice',
+        'view_77' : 'my-purchase-requests'
+    }
+
+    if (page.key in views) {
+        customLoginButton(page.key, views[page.key]);    
+    }
+    
+});
+
+
+// replace "Add New Option" with custom text
+$(document).on('knack-page-render.any', function(event, view) {
+
+  var addNew = $("#kn-input-field_217")
+    .find(".kn-add-option")
+    .html("<i class='fa fa-plus-circle'></i> Add New Vendor | ")
+    .removeClass('kn-add-option')
+    .detach();
+
+  $("#kn-input-field_217").find(".kn-instructions").find("a").before(addNew);
+
+});
+
+
+// --- Begin Item Copying ---
+$(document).on('knack-view-render.view_315', function(event, view) {
+    // automatically submit 'copy' form when modal renders
+    $('button[type=submit]').submit();
+});
+
+$(document).on('knack-form-submit.view_315', function(event, view, record) {
+    // Insert a copy of an item to the same purchase request
+
+    var formUrl = "https://api.knack.com/v1/pages/scene_123/views/view_316/records/";
+
+    // url where to redirect to on record insert success
+    var redirectUrl = "https://atd.knack.com/finance-purchasing#purchase-requests/purchase-request-details/";
+    
+    // grab ID of purchase request and append it to redirect URL
+    console.log(record.field_20_raw);
+
+    redirectUrl = redirectUrl + record.field_20_raw[0].id;
+
+    console.log(redirectUrl);
+    fields = [
+        'field_36', // unit of measure
+        'field_37', // part #
+        'field_15', // description
+        'field_16', // unit cost 
+        'field_20_raw', // purchase request
+        'field_189_raw', // department
+        'field_105_raw', // fund
+        'field_103_raw', // unit
+        'field_104_raw', // object
+    ];
+
+    // reduce object to specified fields
+    const filtered = Object.keys(record)
+      .filter(key => fields.includes(key))
+      .reduce((obj, key) => {
+        var new_key = key.replace('_raw', '')
+        obj[new_key] = record[key];
+        return obj;
+      }, {});
+
+    insertRecord(filtered, formUrl, redirectUrl);
+
+});
+
+
+function insertRecord(record, url, redirectUrl) {
+    
+    Knack.showSpinner();
+
+    var user = Knack.getUserToken();
+
+    var headers = {
+        'X-Knack-Application-ID': '5b422c9b13774837e54ed814',
+        'Authorization': user,
+        'content-type':'application/json'
+    };
+
+    // insert the record
+    $.ajax({
+        url: url,
+        type: 'POST',
+        headers: headers,
+        data:  JSON.stringify(record),
+        success: function(response) {
+          Knack.hideSpinner();
+          window.location = redirectUrl;
+        }
+    });
+
+}
+// --- End Item Copying ---
+
+
+//  redirect to invoice details when invoice created
+//  (mysteriously unable to accomplish this with form submit rule)
+$(document).on('knack-form-submit.view_285', function(event, view, record) {
+  var _id = record.id;  // newly created invoice id
+  var id_pr = record.field_316_raw[0].id; // id of the connected purchase request
+
+  // manually create url of invoice details
+  var url = 'https://atd.knack.com/finance-purchasing#purchase-requests/purchase-request-details/' + id_pr + '/view-invoice-details/' + _id;
+  window.location = url;
+});
+
+
+/**
+ * detect IE
+ * returns version of IE or false, if browser is not Internet Explorer
+ * https://stackoverflow.com/questions/19999388/check-if-user-is-using-ie-with-jquery
+ */
+function detectIE() {
+    var ua = window.navigator.userAgent;
+
+    var msie = ua.indexOf('MSIE ');
+    if (msie > 0) {
+        // IE 10 or older => return version number
+        return parseInt(ua.substring(msie + 5, ua.indexOf('.', msie)), 10);
+    }
+
+    var trident = ua.indexOf('Trident/');
+    if (trident > 0) {
+        // IE 11 => return version number
+        var rv = ua.indexOf('rv:');
+        return parseInt(ua.substring(rv + 3, ua.indexOf('.', rv)), 10);
+    }
+
+    var edge = ua.indexOf('Edge/');
+    if (edge > 0) {
+       // Edge (IE 12+) => return version number
+       return parseInt(ua.substring(edge + 5, ua.indexOf('.', edge)), 10);
+    }
+
+    // other browser
+    return false;
+}
+
+$(document).on('knack-scene-render.scene_1', function() {
+  if (detectIE()) {
+    alert("You are using Microsoft Internet Explorer or Edge to view this page. We recommend using Firefox or Chrome. Contact your system administrator for assistance.");
+  }
+});
